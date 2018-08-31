be57b18a276a2e7627a1373c1e5c926188109dea
vsc31245 <vsc31245@r6i0n13.icts.leuven.vsc>
2018-08-31 15:16:39 +0200

    Add thresholds and regulon info to .loom file

--- R/aux_export2scope.R
+++ R/aux_export2scope.R
@@ -2,7 +2,8 @@
 #' @description Create .loom file with the results of the analysis to visualize them in http://scope.aertslab.org
 #' @param scenicOptions scenicOptions object
 #' @param dgem Digital expression matrix
-#' @return The .loom file (\code{file name indicated in "loomFile" slot in scenicOptions})containing the following information:
+#' @param hierarchy Labels for the hierarchy levels (up to three)
+#' @return The .loom file (\code{file name indicated in "loomFile" slot in scenicOptions}) containing the following information:
 #' \itemize{
 #' \item dgem (digital expression matrix)
 #' \item title: \code{getDatasetInfo(scenicOptions,"datasetTitle")}
@@ -17,13 +18,14 @@
 #' @examples 
 #' # devtools::install_github("aertslab/SCopeLoomR")
 #' # scenicOptions@fileNames$output["loomFile",] <- "myAnalysis.loom"
-#' # export2scope(scenicOptions, exprMat)
+#' # export2scope(scenicOptions, exprMat, hierarchy=c("SCENIC", "MouseBrain"))
 #' @export 
-export2scope <- function(scenicOptions, dgem)
+export2scope <- function(scenicOptions, dgem, hierarchy=c("SCENIC", "", ""), addAllTsnes=TRUE)
 {
   # TODO: what about if there is no dgem, but only normalized?
-  # TODO: Error File 'int/cellInfo.Rds' does not exist.
   
+  if(length(hierarchy) > 3) stop("Maximum three hierarchy levels")
+  if(length(hierarchy) < 3) hierarchy <- c(hierarchy, rep("", 5-length(hierarchy)))
   
   # TODO: ask max about order of samples tsne-expr-info
   suppressPackageStartupMessages(require(SCopeLoomR))
@@ -42,37 +44,56 @@ export2scope <- function(scenicOptions, dgem)
                      default.embedding=defaultTsne,
                      default.embedding.name=defaultTsne_name)
   
+  add_hierarchy(loom=loom, hierarchy=create_hierarchy(level.1.name=hierarchy[1], 
+                                                      level.2.name=hierarchy[2], 
+                                                      level.3.name=hierarchy[3]))
+  
   # Known cell information/annotation  
   cellInfo <- loadFile(scenicOptions, getDatasetInfo(scenicOptions, "cellInfo"), ifNotExists="null")
   if(!is.null(cellInfo))
   {
     cellInfo <- data.frame(cellInfo)
     cellInfo <- cellInfo[,colnames(cellInfo) != "nGene", drop=FALSE]
-  }
   
-  # Add annotation
-  for(cn in colnames(cellInfo))
-  {
-    add_col_attr(loom=loom, key=cn, value=cellInfo[,cn])
+    # Add annotation
+    for(cn in colnames(cellInfo))
+    {
+      add_col_attr(loom=loom, key=cn, value=cellInfo[,cn])
+    }
   }
-  # cellInfo$nGene <- colSums(dgem>0)
-  # add_col_attr(loom=loom, key = "nGene", value=cellInfo$nGene)
-  
   
   # Regulons AUC matrix
   regulonAUC <- loadInt(scenicOptions, "aucell_regulonAUC")
-  add_scenic_regulons_auc_matrix(loom=loom, regulons.AUC=getAUC(regulonAUC))
+  add_scenic_regulons_auc_matrix(loom=loom, regulons.AUC=AUCell::getAUC(regulonAUC))
   
   # Regulons (gene list)
   regulons <- loadInt(scenicOptions, "aucell_regulons")
-  add_scenic_regulons(loom=loom, dgem=dgem, regulons=regulons)
-  
-  # Add regulons thresholds #TODO
-  # add_global_md_regulon_thresholds(loom=loom, regulon.threshold.assignments=...$threshold.assignment)
+  motifEnrichment <- loadInt(scenicOptions, "motifEnrichment_selfMotifs_wGenes")
+  regulonThresholds <- loadInt(scenicOptions, "aucell_thresholds")
+  add_scenic_regulons(loom=loom
+                      , dgem=dgem
+                      , regulons=regulons
+                      , regulon.threshold.assignments=regulonThresholds # Optional
+                      , regulon.enrichment.table=motifEnrichment # Optional
+                      )
   
   # # Alternative t-SNE #TODO
   # load(paste0(scenicDir, "int/3.3_tsneRegulonAUC_PCA_50pc50perpl.RData"))
   # add_embedding(loom=loom, embedding=tSNE$Y, name="SCENIC (t-SNE on AUC)")
+  if(addAllTsnes)
+  {
+    tsnePath <- dirname(getSettings(scenicOptions, "tSNE_filePrefix"))
+    allTsneFiles <- grep(pattern = paste0(basename(getSettings(scenicOptions, "tSNE_filePrefix")), ".*\\.Rds")
+         , x=list.files(tsnePath)
+         , value = T)
+    
+    for(tsneFile in allTsneFiles)
+    {
+      tryCatch(
+      add_embedding(loom=loom, embedding=readRDS(file.path(tsnePath, tsneFile))$Y, name=paste0("SCENIC: ", file.path(tsneFile)))
+      , error=function(e) print(paste0("Cannot add tsne: ", e$message)))
+    }
+  }
   
   finalize(loom=loom)
 }
